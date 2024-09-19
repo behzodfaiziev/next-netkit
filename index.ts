e470ecec7a9bd774ecddb406fe36adf7292fe243
Behzod <behzod.faiziev@gmail.com>
2024-09-19 21:16:39 +0300

    v 0.0.9

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { NetworkManager } from "./src/network-manager";
-import type {INetworkManager} from "./src/network-manager.interface";
-import {NetworkErrorParams} from "./src/interfaces/network-error-params";
+import 'reflect-metadata';
+import { NetworkManager } from "@/network-manager";
+import type { INetworkManager } from "@/network-manager.interface";
+import { NetworkErrorParams } from "@/interfaces/network-error-params";
 
-
-export {INetworkManager, NetworkManager, NetworkErrorParams };
+export { INetworkManager, NetworkManager, NetworkErrorParams };
