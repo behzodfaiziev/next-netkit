df187fdc416addcd897b7518eaaa67c939a79029
Behzod <behzod.faiziev@gmail.com>
2024-09-19 17:05:29 +0300

    v 0.0.6

--- src/error/api-exception.ts
+++ src/error/api-exception.ts
@@ -1,4 +1,4 @@
-import type {NetworkErrorParams} from "@/interfaces/network-error-params";
+import type {NetworkErrorParams} from "../interfaces/network-error-params";
 
 type JsonType = string | { [key: string]: unknown };
 
